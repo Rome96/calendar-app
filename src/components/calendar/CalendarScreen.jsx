82393d1d2a21419dcb29af79ffd71dd3f789c740
Turiano Romero <romero.guzman18@gmail.com>
2021-01-06 19:33:19 -0500

    creando y configurando calendario usando big calendar y agg styles

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 import Navbar from '../ui/Navbar';
 
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+  title: 'Cumpleaños de mi madre',
+  start: moment().toDate(), // new Date()
+  end: moment().add(2, 'hours').toDate(),
+  bgColor: 'FAFAFA',
+}]
+
 const CalendarScreen = () => {
-  return <div>
-    <Navbar />
-    <h2>Calendar App</h2>
-  </div>
+  return (
+    <div className="calendar-screen">
+      <Navbar />
+      <h2>Calendar App</h2>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+      />
+    </div>
+  );
 };
 
 export default CalendarScreen;
