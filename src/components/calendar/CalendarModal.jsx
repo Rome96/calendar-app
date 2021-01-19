29f319e82d99bc7a1e57a13a3fae994fdb9c820b
Turiano Romero <romero.guzman18@gmail.com>
2021-01-19 17:24:57 -0500

    modal content

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -12,19 +12,65 @@ const CalendarModal = () => {
     setOpenModal(false)
   };
 
-  return <Modal
-    isOpen={openModal}
-    style={customStyles}
-    className="modal"
-    overlayClassName="modal-background"
-    closeTimeoutMS={200}
-    // onAfterOpen={afterOpenModal}
-    onRequestClose={closeModal}
-    contentLabel="Example Modal"
-  >
-    <h1>Hola universo</h1> <hr/>
-    <span>Como anda todo ?</span>
-  </Modal>
+  return (
+    <Modal
+      isOpen={openModal}
+      style={customStyles}
+      className="modal"
+      overlayClassName="modal-background"
+      closeTimeoutMS={200}
+      // onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      contentLabel="Example Modal"
+    >
+      <h1>Nuevo evento</h1>
+      <hr />
+      <form className="container">
+        <div className="form-group">
+          <label>Fecha y hora inicio</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <div className="form-group">
+          <label>Fecha y hora fin</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <hr />
+        <div className="form-group">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autocomplete="off"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
+    </Modal>
+  );
 };
 
 export default CalendarModal;
